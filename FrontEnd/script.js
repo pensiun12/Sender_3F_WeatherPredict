52de7946a8a04905d37de76eafe1a98025907b70
Zeinalfakih <90429940+Zeinalfakih@users.noreply.github.com>
2025-05-20 17:01:51 +0700

    file javscript

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -4,81 +4,80 @@ function updateWeather() {
     alert("Fitur pencarian untuk kota " + city + " akan dikembangkan.");
     // Di sini bisa ditambahkan pemanggilan API cuaca nyata di masa depan
   }
-  
-const hourlyData = [
-    { time: "01:00", icon: "☁️", temp: "20°", rain: "0%" },
-    { time: "02:00", icon: "☁️", temp: "20°", rain: "0%" },
-    { time: "03:00", icon: "☁️", temp: "20°", rain: "0%" },
-    { time: "04:00", icon: "☁️", temp: "20°", rain: "0%" },
-    { time: "05:00", icon: "☁️", temp: "20°", rain: "0%" },
-    { time: "06:00", icon: "🌦️", temp: "30°", rain: "20%" },
-    { time: "07:00", icon: "🌦️", temp: "30°", rain: "20%" },
-    { time: "08:00", icon: "🌦️", temp: "30°", rain: "20%" },
-    { time: "09:00", icon: "🌦️", temp: "30°", rain: "20%" },
-    { time: "10:00", icon: "🌦️", temp: "30°", rain: "20%" },
-    { time: "11:00", icon: "🌦️", temp: "30°", rain: "20%" },
-    { time: "12:00", icon: "🌦️", temp: "30°", rain: "20%" },
-    { time: "13:00", icon: "🌦️", temp: "30°", rain: "20%" },
-    { time: "14:00", icon: "🌦️", temp: "30°", rain: "20%" },
-    { time: "15:00", icon: "🌦️", temp: "30°", rain: "20%" },
-    { time: "16:00", icon: "🌦️", temp: "30°", rain: "20%" },
-    { time: "17:00", icon: "🌦️", temp: "30°", rain: "20%" },
-    { time: "18:00", icon: "🌦️", temp: "30°", rain: "20%" },
-    { time: "19:00", icon: "🌦️", temp: "30°", rain: "20%" },
-    { time: "20:00", icon: "🌦️", temp: "30°", rain: "20%" },
-    { time: "21:00", icon: "🌦️", temp: "30°", rain: "20%" },
-    { time: "22:00", icon: "🌦️", temp: "30°", rain: "20%" },
-    { time: "23:00", icon: "🌦️", temp: "30°", rain: "20%" },
-    { time: "00:00", icon: "🌦️", temp: "30°", rain: "20%" },
+  const hourlyData = [
+    { time: "01:00", icon: '<img src="assets/sunny.png" alt="cloudy" class="icon-img">', temp: "20°", rain: "0%" },
+    { time: "02:00", icon: '<img src="assets/sunny.png" alt="cloudy" class="icon-img">', temp: "20°", rain: "0%" },
+    { time: "03:00", icon: '<img src="assets/sunny.png" alt="cloudy" class="icon-img">', temp: "20°", rain: "0%" },
+    { time: "04:00", icon: '<img src="assets/sunny.png" alt="cloudy" class="icon-img">', temp: "20°", rain: "0%" },
+    { time: "05:00", icon: '<img src="assets/sunny.png" alt="cloudy" class="icon-img">', temp: "20°", rain: "0%" },
+    { time: "06:00", icon: '<img src="assets/sunny.png" alt="cloudy" class="icon-img">', temp: "30°", rain: "20%" },
+    { time: "07:00", icon: '<img src="assets/sunny.png" alt="cloudy" class="icon-img">', temp: "30°", rain: "20%" },
+    { time: "08:00", icon: '<img src="assets/sunny.png" alt="cloudy" class="icon-img">', temp: "30°", rain: "20%" },
+    { time: "09:00", icon: '<img src="assets/sunny.png" alt="cloudy" class="icon-img">', temp: "30°", rain: "20%" },
+    { time: "10:00", icon: '<img src="assets/sunny.png" alt="cloudy" class="icon-img">', temp: "30°", rain: "20%" },
+    { time: "11:00", icon: '<img src="assets/sunny.png" alt="cloudy" class="icon-img">', temp: "30°", rain: "20%" },
+    { time: "12:00", icon: '<img src="assets/sunny.png" alt="cloudy" class="icon-img">', temp: "30°", rain: "20%" },
+    { time: "13:00", icon: '<img src="assets/sunny.png" alt="cloudy" class="icon-img">', temp: "30°", rain: "20%" },
+    { time: "14:00", icon: '<img src="assets/sunny.png" alt="cloudy" class="icon-img">', temp: "30°", rain: "20%" },
+    { time: "15:00", icon: '<img src="assets/sunny.png" alt="cloudy" class="icon-img">', temp: "30°", rain: "20%" },
+    { time: "16:00", icon: '<img src="assets/sunny.png" alt="cloudy" class="icon-img">', temp: "30°", rain: "20%" },
+    { time: "17:00", icon: '<img src="assets/sunny.png" alt="cloudy" class="icon-img">', temp: "30°", rain: "20%" },
+    { time: "18:00", icon: '<img src="assets/sunny.png" alt="cloudy" class="icon-img">', temp: "30°", rain: "20%" },
+    { time: "19:00", icon: '<img src="assets/sunny.png" alt="cloudy" class="icon-img">', temp: "30°", rain: "20%" },
+    { time: "20:00", icon: '<img src="assets/sunny.png" alt="cloudy" class="icon-img">', temp: "30°", rain: "20%" },
+    { time: "21:00", icon: '<img src="assets/sunny.png" alt="cloudy" class="icon-img">', temp: "30°", rain: "20%" },
+    { time: "22:00", icon: '<img src="assets/sunny.png" alt="cloudy" class="icon-img">', temp: "30°", rain: "20%" },
+    { time: "23:00", icon: '<img src="assets/sunny.png" alt="cloudy" class="icon-img">', temp: "30°", rain: "20%" },
+    { time: "00:00", icon: '<img src="assets/sunny.png" alt="cloudy" class="icon-img">', temp: "30°", rain: "20%" },
   ];
   
   // Data prakiraan harian
   const dailyData = [
     {
       day: "Jumat, 25 April",
-      icon: "🌦️",
+      icon: '<img src="assets/sunny.png" alt="cloudy" class="icon-daily-img">',
       desc: "Sebagian Hujan",
       temp: "32°C",
       wind: "Angin pada 15 hingga 25 km/jam",
     },
     {
       day: "Sabtu, 26 April",
-      icon: "☀️",
+      icon: '<img src="assets/sunny.png" alt="cloudy" class="icon-daily-img">',
       desc: "Cerah",
       temp: "32°C",
       wind: "Angin pada 15 hingga 25 km/jam",
     },
     {
       day: "Minggu, 27 April",
-      icon: "🌧️",
+      icon: '<img src="assets/sunny.png" alt="cloudy" class="icon-daily-img">',
       desc: "Hujan",
       temp: "32°C",
       wind: "Angin pada 15 hingga 25 km/jam",
     },
     {
       day: "Senin, 28 April",
-      icon: "🌤️",
+      icon: '<img src="assets/sunny.png" alt="cloudy" class="icon-daily-img">',
       desc: "Cerah Berawan",
       temp: "30°C",
       wind: "Angin pada 15 hingga 25 km/jam",
     },
     {
       day: "Selasa, 29 April",
-      icon: "🌧️",
+      icon: '<img src="assets/sunny.png" alt="cloudy" class="icon-daily-img">',
       desc: "Hujan",
       temp: "30°C",
       wind: "Angin pada 15 hingga 25 km/jam",
     },
     {
         day: "Rabu, 30 April",
-        icon: "🌧️",
+        icon: '<img src="assets/sunny.png" alt="cloudy" class="icon-daily-img">',
         desc: "Hujan",
         temp: "30°C",
         wind: "Angin pada 15 hingga 25 km/jam",
       },
     {
         day: "Kamis, 31 April",
-        icon: "🌧️",
+        icon: '<img src="assets/sunny.png" alt="cloudy" class="icon-daily-img">',
         desc: "Hujan",
         temp: "30°C",
         wind: "Angin pada 15 hingga 25 km/jam",
@@ -164,5 +163,21 @@ btnRightDaily.addEventListener("click", () => {
       return;
     }
     alert(`Fitur pencarian untuk kota ${city} akan dikembangkan.`);
-  }
+}
+//Toggle class active
+const topNav = document.querySelector('.top-menu')
+
+//Hamburger menu di klik
+document.querySelector('#hamburger-menu').onclick =()=>{
+    topNav.classList.toggle('active');
+};
+
+//klik diluar untuk menghilangkan sidebar
+const Hamburger = document.querySelector('#hamburger-menu');
+
+document.addEventListener('click', function(e){
+    if(!Hamburger.contains(e.target) && !topNav.contains(e.target)){
+        topNav.classList.remove('active');
+    }
+})
   
