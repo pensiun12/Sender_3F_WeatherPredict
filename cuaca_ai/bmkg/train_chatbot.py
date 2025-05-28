97a611f75c4c9155550aebffc0a6c2b6ec172494
ferryoga <albertalbert19987@gmail.com>
2025-05-29 01:02:45 +0700

    load data intents

--- cuaca_ai/bmkg/train_chatbot.py
+++ cuaca_ai/bmkg/train_chatbot.py
@@ -7,3 +7,29 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout
 from tensorflow.keras.optimizers import SGD
 import pickle
+
+# Inisialisasi
+lemmatizer = WordNetLemmatizer()
+
+# Load data intents
+with open('bmkg\intents.json',  encoding='utf-8') as file:
+    data = json.load(file)
+
+words = []
+classes = []
+documents = []
+ignore_letters = ['?', '!', '.', ',']
+
+# Tokenisasi dan lemmatization
+for intent in data['intents']:
+    for pattern in intent['patterns']:
+        word_list = nltk.word_tokenize(pattern)
+        words.extend(word_list)
+        documents.append((word_list, intent['tag']))
+        if intent['tag'] not in classes:
+            classes.append(intent['tag'])
+
+# Normalisasi kata
+words = [lemmatizer.lemmatize(w.lower()) for w in words if w not in ignore_letters]
+words = sorted(set(words))
+classes = sorted(set(classes))
