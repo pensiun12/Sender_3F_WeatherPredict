5de0f953d65fb77e3e34221d61fa7ffb27f0e777
ferryoga <albertalbert19987@gmail.com>
2025-05-29 01:01:25 +0700

    train chatbot

---
+++ cuaca_ai/bmkg/train_chatbot.py
@@ -0,0 +1,9 @@
+import json
+import numpy as np
+import random
+import nltk
+from nltk.stem import WordNetLemmatizer
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout
+from tensorflow.keras.optimizers import SGD
+import pickle
