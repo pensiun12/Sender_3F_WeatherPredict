aea7c5388333851b893ed0dbbab22f5d81d8ef08
ferryoga <ferrypratama9573@gmail.com>
2025-05-29 09:19:53 +0700

    day

--- cuaca_ai/bmkg/chatbot_response.py
+++ cuaca_ai/bmkg/chatbot_response.py
@@ -62,3 +62,15 @@ def extract_city(text):
         if two_words in df_kota['city_name_normalized'].values:
             return two_words
     return None
+
+def extract_day_offset(text):
+    text = text.lower()
+    if "lusa" in text:
+        return 2
+    elif "besok" in text:
+        return 1
+    elif "kemarin" in text:
+        return -1
+    elif "hari ini" in text or "sekarang" in text:
+        return 0
+    return 0  # default
