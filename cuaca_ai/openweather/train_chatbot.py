f59719da2f929fc38e624119bd60e3b2df2e0714
The1things <albertalbert19987@gmail.com>
2025-05-29 09:40:59 +0700

    train chatbot

---
+++ cuaca_ai/openweather/train_chatbot.py
@@ -0,0 +1,82 @@
+import json
+import numpy as np
+import random
+import nltk
+from nltk.stem import WordNetLemmatizer
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout
+from tensorflow.keras.optimizers import SGD
+import pickle
+
+# Inisialisasi
+lemmatizer = WordNetLemmatizer()
+
+# Load data intents
+with open('openweather\intents.json',  encoding='utf-8') as file:
+    data = json.load(file)
+
+words = []
+classes = []
+documents = []
+ignore_letters = ['?', '!', '.', ',']
+
+# Tokenisasi dan lemmatization
+for intent in data['intents']:
+    for pattern in intent['patterns']:
+        word_list = nltk.word_tokenize(pattern)
+        words.extend(word_list)
+        documents.append((word_list, intent['tag']))
+        if intent['tag'] not in classes:
+            classes.append(intent['tag'])
+
+# Normalisasi kata
+words = [lemmatizer.lemmatize(w.lower()) for w in words if w not in ignore_letters]
+words = sorted(set(words))
+classes = sorted(set(classes))
+
+# Simpan kata dan kelas
+pickle.dump(words, open('openweather/words.pkl', 'wb'))
+pickle.dump(classes, open('openweather/classes.pkl', 'wb'))
+
+# Membuat data training
+training = []
+output_empty = [0] * len(classes)
+
+for doc in documents:
+    bag = []
+    word_patterns = doc[0]
+    word_patterns = [lemmatizer.lemmatize(w.lower()) for w in word_patterns]
+    for w in words:
+        bag.append(1) if w in word_patterns else bag.append(0)
+
+    output_row = list(output_empty)
+    output_row[classes.index(doc[1])] = 1
+
+    training.append([bag, output_row])
+
+# Shuffle dan ubah ke numpy array
+random.shuffle(training)
+training = np.array(training, dtype=object)
+
+train_x = np.array(list(training[:, 0]))
+train_y = np.array(list(training[:, 1]))
+
+# Bangun model
+model = Sequential()
+model.add(Dense(128, input_shape=(len(train_x[0]),), activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(64, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(len(train_y[0]), activation='softmax'))
+
+# Compile
+sgd = SGD(learning_rate=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+
+# Training
+model.fit(train_x, train_y, epochs=200, batch_size=5, verbose=1)
+
+# Simpan model
+model.save('openweather/chatbot_model.h5')
+
+print("Model selesai dilatih dan disimpan sebagai chatbot_model.h5")
