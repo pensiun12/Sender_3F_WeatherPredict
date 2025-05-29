aaf3884d9b5cdb7e6b3a780c3c1b1a209b8033e5
ferryoga <ferrypratama9573@gmail.com>
2025-05-29 08:00:30 +0700

    compile

--- cuaca_ai/bmkg/train_chatbot.py
+++ cuaca_ai/bmkg/train_chatbot.py
@@ -68,3 +68,7 @@ model.add(Dropout(0.5))
 model.add(Dense(64, activation='relu'))
 model.add(Dropout(0.5))
 model.add(Dense(len(train_y[0]), activation='softmax'))
+
+# Compile
+sgd = SGD(learning_rate=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
