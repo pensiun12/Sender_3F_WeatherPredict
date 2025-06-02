a86bf3080acdb97093853a3bbad9354e40e2d4b9
RyanPrasetya12 <ryanprasetya3031@gmail.com>
2025-06-02 09:43:32 +0700

    api untuk chatbot

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-# from openweather.chatbot_response import chatbot_response, initial_greeting_with_weather
+#from openweather.chatbot_response import chatbot_response, initial_greeting_with_weather
 from weatherapi.chatbot_response import chatbot_response, initial_greeting_with_weather
 
 app = Flask(__name__)
